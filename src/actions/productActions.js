4c6760386d3a0d5ac701c553ca1dc4d695e9134f
shemaM <azertshema@gmail.com>
2021-04-12 23:58:58 +0200

    ft(product): listing  and deleting product

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
@@ -5,6 +6,9 @@ import {
   LIST_PRODUCT_FAIL,
   LIST_PRODUCT_SUCCESS,
   LIST_PRODUCT_REQUEST,
+  DELETE_PRODUCT_REQUEST,
+  DELETE_PRODUCT_SUCCESS,
+  DELETE_PRODUCT_FAIL,
 } from '../constants/productConstants';
 
 const url = 'http://localhost:4000/product/';
@@ -21,7 +25,7 @@ export const listProduct = () => async (dispatch, getState) => {
 
     const config = {
       headers: {
-        Authorization: `${userData}`,
+        Authorization: `Bearer ${userData}`,
       },
     };
 
@@ -29,11 +33,40 @@ export const listProduct = () => async (dispatch, getState) => {
 
     dispatch({
       type: LIST_PRODUCT_SUCCESS,
-      payload: data,
+      payload: data.data,
     });
   } catch (error) {
     dispatch({
       type: LIST_PRODUCT_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+export const productDelete = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: DELETE_PRODUCT_REQUEST,
+    });
+
+    const {
+      userLogin: { userData },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userData}`,
+      },
+    };
+
+    await axios.delete(`${url}/delete/${id}`, config);
+
+    dispatch({
+      type: DELETE_PRODUCT_SUCCESS,
+    });
+  } catch (error) {
+    dispatch({
+      type: DELETE_PRODUCT_FAIL,
       payload: error.response.data.message,
     });
   }
--- src/screens/DashboardScreens/ProductListScreen.js
+++ src/screens/DashboardScreens/ProductListScreen.js
@@ -1,91 +1,104 @@
-import React from 'react';
+/* eslint-disable no-alert */
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Trash, Edit } from 'react-feather';
+import { useDispatch, useSelector } from 'react-redux';
 import NavBar from '../../components/Header';
 import Sidebar from '../../components/Sidebar';
+import { listProduct, productDelete } from '../../actions/productActions';
 
-const ProductListScreen = () => (
-  <>
-    <header>
-      <NavBar />
-    </header>
-    <main>
-      <div className='bg-gray-50 mt-12 grid grid-cols-4'>
-        <div className='bg-gray-900 h-screen w-64 fixed'>
-          <Sidebar />
-        </div>
-        <div className='col-span-6 h-screen border-2 ml-64 px-20 py-5'>
-          <div className='flex justify-between'>
-            <h1 className='text-2xl'>PRODUCTS</h1>
-            <Link
-              to='/product_create'
-              className='text-xs border-gray-700 border-2 px-2 py-2 text-gray-200 bg-black'
-            >
-              +CREATE PRODUCT
-            </Link>
+const ProductListScreen = () => {
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  const deleteProduct = useSelector((state) => state.productList);
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = deleteProduct;
+
+  const deleteHandler = (id) => {
+    if (window.confirm('Are you sure')) {
+      dispatch(productDelete(id));
+    }
+  };
+
+  useEffect(() => {
+    dispatch(listProduct());
+  }, [dispatch, successDelete]);
+
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <div className='bg-gray-50 mt-12 grid grid-cols-4'>
+          <div className='bg-gray-900 h-screen w-64 fixed'>
+            <Sidebar />
           </div>
-          <div className='mt-5'>
-            <table className='table-auto w-full'>
-              <thead>
-                <tr className='border-gray-200 border-2'>
-                  <th className='text-left py-2'>ID</th>
-                  <th className='text-left py-2'>NAME</th>
-                  <th className='text-left py-2'>DESCRIPTION</th>
-                  <th className='text-left py-2'>BRAND</th>
-                  <th className='text-left py-2'>CATEGORY</th>
-                  <th className='text-left py-2'> </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr className='border-gray-200 border-2'>
-                  <td className='py-2'>1</td>
-                  <td className='py-2'>Iphone 12</td>
-                  <td className='py-2'>8GB-RAM 64GB-MEMORY</td>
-                  <td className='py-2'>Apple</td>
-                  <td className='py-2'>Phones</td>
-                  <td>
-                    <ul className='flex justify-center'>
-                      <li>
-                        <Link to='/edit_product'>
-                          <Edit className='border-white py-1 px-1 text-2xl  bg-white border-2' />
-                        </Link>
-                      </li>
-                      <li>
-                        <button type='button'>
-                          <Trash className='border-red-500 py-1 px-1 bg-red-500 border-2' />
-                        </button>
-                      </li>
-                    </ul>
-                  </td>
-                </tr>
-                <tr className='border-gray-200 bg-red-50 border-2'>
-                  <td className='py-2'>1</td>
-                  <td className='py-2'>Iphone 12</td>
-                  <td className='py-2'>8GB-RAM 64GB-MEMORY</td>
-                  <td className='py-2'>Apple</td>
-                  <td className='py-2'>Phones</td>
-                  <td>
-                    <ul className='flex justify-center'>
-                      <li>
-                        <Link to='/edit_product'>
-                          <Edit className='border-white py-1 px-1 text-2xl  bg-white border-2' />
-                        </Link>
-                      </li>
-                      <li>
-                        <button type='button'>
-                          <Trash className='border-red-500 py-1 px-1 bg-red-500 border-2' />
-                        </button>
-                      </li>
-                    </ul>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+          <div className='col-span-6 h-screen border-2 ml-64 px-20 py-5'>
+            <div className='flex justify-between'>
+              <h1 className='text-2xl'>PRODUCTS</h1>
+              <Link
+                to='/product_create'
+                className='text-xs border-gray-700 border-2 px-2 py-2 text-gray-200 bg-black'
+              >
+                +CREATE PRODUCT
+              </Link>
+            </div>
+            <div className='mt-5'>
+              <table className='table-auto w-full'>
+                <thead>
+                  <tr className='border-gray-200 border-2'>
+                    <th className='text-left py-2'>ID</th>
+                    <th className='text-left py-2'>NAME</th>
+                    <th className='text-left py-2'>DESCRIPTION</th>
+                    <th className='text-left py-2'>BRAND</th>
+                    <th className='text-left py-2'>CATEGORY</th>
+                    <th className='text-left py-2'> </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products.map((product) => (
+                    <tr className='border-gray-200 border-2'>
+                      <td className='py-2'>{product.id}</td>
+                      <td className='py-2'>{product.name}</td>
+                      <td className='py-2'>{product.description}</td>
+                      <td className='py-2'>{product.brand}</td>
+                      <td className='py-2'>{product.category}</td>
+                      <td>
+                        <ul className='flex justify-center'>
+                          <li>
+                            <Link to='/edit_product'>
+                              <Edit className='border-white py-1 px-1 text-2xl  bg-white border-2' />
+                            </Link>
+                          </li>
+                          <li>
+                            <button
+                              type='button'
+                              onClick={() => deleteHandler(product.id)}
+                            >
+                              <Trash className='border-red-500 py-1 px-1 bg-red-500 border-2' />
+                            </button>
+                          </li>
+                        </ul>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
-  </>
-);
+      </main>
+    </>
+  );
+};
 
 export default ProductListScreen;
